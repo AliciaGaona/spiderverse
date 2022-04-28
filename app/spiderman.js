827ee1be3d47c9891ef0e26a6d725d056194e360
AliciaGaona <aligaovela@gmail.com>
2022-04-28 01:52:52 -0500

    Se crea test para objeto spiderman, que falle

---
+++ app/spiderman.js
@@ -0,0 +1,12 @@
+class Spiderman{
+    constructor(nombre,edad,actor,numeroPeliculas,estudioCine){
+        this.nombre=nombre
+        this.edad=edad
+        this.actor=actor
+        this.numeroPeliculas=numeroPeliculas
+        this.estudioCine=estudioCine
+    }
+}
+
+//exporto mi clase
+module.exports = Spiderman
--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,25 @@
+//const Spiderman = require("../app/spiderman");
+
 describe("Test Suite Dummy Description", () => {
     test('Case 1 Dummy', () => {
       const resultOfSomething = 1 + 2
-      expect(resultOfSomething).toBe(18);
+      expect(resultOfSomething).toBe(32);
     });
   })
+
+  describe("Unit Test for Spiderman class", () => {
+    test('1) Create an spiderman object', () => {
+      //escribimos el código que queremos usar tal cuál
+      //Quiero poder instanciar un objeto Spiderman con esta info
+      const andrewGarfield= new Spiderman ("Spiderman Sony", 31, "Andrew Garfield",2,"Sony")
+
+      //validamos que este código funcione de la forma esperada
+      expect(andrewGarfield.nombre).toBe("Spiderman Sony")
+      expect(andrewGarfield.edad).toBe(31)
+      expect(andrewGarfield.actor).toBe("SAndrew Garfield")
+      expect(andrewGarfield.numeroPeliculas).toBe(31)
+      expect(andrewGarfield.estudioCine).toBe("SAndrew Garfield")
+    });
+  })
+
+
